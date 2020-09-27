3bf3edf4b068907dc9082a9ceb8cbb5c62c8309b
mrajani <laltopi@gmail.com>
2020-09-26 22:40:59 -0700

    initial commit

---
+++ install_vault.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+# Install Vault Server on Linux Platform 
+
+tag=${1:-1.5.4}
+vault=vault_${tag}_linux_amd64.zip
+url=https://releases.hashicorp.com/vault/${tag}/${vault}
+echo "Downloading ${tag} version of Vault"
+wget -c -q ${url} && sudo unzip -d /usr/bin ${vault} && rm ${vault} 
+
+# Add vault user and group
+sudo useradd -r -d /opt/vault/vault-data -s /bin/nologin vault
+sudo install -o vault -g vault -m 750 -d /opt/vault/vault-data
+
+# creat vault director in /etc
+sudo mkdir -p /etc/vault
+# create log directory for vault
+sudo mkdir -p /var/log/vault
+sudo install -o vault -g vault -m 750 -d /var/log/vault
+
+# copy vault.service to systemd
+sudo cp config.hcl /etc/vault/
+sudo cp vault.service /etc/systemd/system/vault.service
+sudo systemctl deamon-reload
+sudo systemctl start vault.service
+
